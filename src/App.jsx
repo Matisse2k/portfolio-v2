ce7ec4346a506e12506682d38d669d2194518648
Matisse Ben Addi <matissegreetz@gmail.com>
2026-02-17 19:36:29 +0100

    Initial commit: Set up Vite + React portfolio project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div className="portfolio">
+    <h1>Mijn Portfolio</h1>
+    <p>Welkom op mijn portfolio website</p>
+   </div>
+  )
+}
+
+export default App
